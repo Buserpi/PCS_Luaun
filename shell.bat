cb0293bff5bb80389abcc36d821e1c4c7abf91e9
瓦特桥谷物 <46279874+qiaoliangXgamemode@users.noreply.github.com>
2021-07-01 14:57:03 +0800

    Add files via upload

---
+++ shell.bat
@@ -0,0 +1,4 @@
+git config --global user.email "pppwwi@qq.com"
+git config --global user.name "qiaoliangXgamemode"
+ssh-keygen -t rsa -C "pppwwi@qq.com"
+git clone https://github.com/Buserpi/PCS_Luaun.git
